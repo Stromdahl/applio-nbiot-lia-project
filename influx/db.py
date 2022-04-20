0dcb511f3163db26cccb5084d3336f17f4dd7c8c
David Ka <david.karnel@student.kyh.se>
2022-04-20 15:00:18 +0200

    env file added

--- influx/db.py
+++ influx/db.py
@@ -1,18 +1,18 @@
 import os
 
+from flask.cli import load_dotenv
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+load_dotenv()
 
-influx_cloud_url = "url"
-influx_cloud_token = os.getenv(
-    "token")
-influx_cloud_token = 'token'
-bucket = "bucket"
-org = "org"
+token = os.getenv('token')
+org = os.getenv('org')
+bucket = os.getenv('bucket')
+url = os.getenv('url')
 
 
-client = InfluxDBClient(url=influx_cloud_url, token=influx_cloud_token, org=org, verify_ssl=None)
+client = InfluxDBClient(url=url, token=token, org=org, verify_ssl=None)
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 query_api = client.query_api()
@@ -21,14 +21,10 @@ p = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
 
 write_api.write(bucket=bucket, record=p)
 
-
 ## using Table structure
 tables = query_api.query('from(bucket:"testraw") |> range(start: -10m)')
 
 for table in tables:
     print(table)
     for row in table.records:
-        print (row.values)
-
-
-
+        print(row.values)
