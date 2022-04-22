7ec154406d2c3b1a703f91d66c1de70af15b7578
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2022-04-22 11:55:53 +0200

    test mqtt stuff

--- nbiot-server/src/test.py
+++ nbiot-server/src/test.py
@@ -1,8 +1,40 @@
+import paho.mqtt.client as mqtt
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code " + str(rc))
+
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	client.subscribe("test/#")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+	print(msg.topic + " " + str(msg.payload))
+
+
+def on_publish(client, userdata, result):
+	print("Data published...")
+
 
 def main():
-	from config import Config
-	print('ADDRESS	{}'.format(Config.ADDRESS))
-	print('PORT		{}'.format(Config.PORT))
+	client = mqtt.Client()
+	client.on_connect = on_connect
+	client.on_message = on_message
+	client.on_publish = on_publish
+
+#	client.connect("mqtt.eclipseprojects.io", 1883, 60)
+
+	client.connect("127.0.0.1", 1883, 60)
+	ret = client.publish("test/python")
+
+	# Blocking call that processes network traffic, dispatches callbacks and
+	# handles reconnecting.
+	# Other loop*() functions are available that give a threaded interface and a
+	# manual interface.
+	client.loop_forever()
 
 
 if __name__ == '__main__':
